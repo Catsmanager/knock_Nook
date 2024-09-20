6e8532945b618b1c193827ce957544cef752f299
mosy2266 <mosy2266@naver.com>
2024-09-20 20:32:52 +0900

    gitignore 수정

---
+++ knocknook/src/main/java/com/hnn/knocknook/KnocknookApplication.java
@@ -0,0 +1,13 @@
+package com.hnn.knocknook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnocknookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnocknookApplication.class, args);
+	}
+
+}
---
+++ knocknook/src/test/java/com/hnn/knocknook/KnocknookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnn.knocknook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnocknookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
