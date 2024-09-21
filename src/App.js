1672a83d440940f89691fede790013f29522d1cb
Hyunji <hyunhyun0096@naver.com>
2024-09-21 14:31:39 +0900

    hyunjifront

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import Page2 from './Page2';
+import Page3 from './Page3';
+
+function App() {
+  const [markerPosition, setMarkerPosition] = useState(null); // 좌표 정보 상태
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route 
+          path="/page2" 
+          element={<Page2 setMarkerPosition={setMarkerPosition} />} // Page2에서 좌표 설정
+        />
+        <Route 
+          path="/page3" 
+          element={<Page3 markerPosition={markerPosition} />} // Page3에 좌표 전달
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ src/HomePage.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Container, Text } from 'retro-react';
+import './App.css';
+
+function HomePage() {
+  const navigate = useNavigate();
+  const [startWalking, setStartWalking] = useState(false);
+
+  const handleButtonClick = () => {
+    setStartWalking(true); 
+
+   
+    setTimeout(() => {
+      navigate('/page2');
+    }, 1000);
+  };
+
+  return (
+    <div className={`App ${startWalking ? 'walking' : ''}`}>
+
+      <video autoPlay loop muted className="video-background">
+        <source src={`${process.env.PUBLIC_URL}/background.mp4`} type="video/mp4" />
+        지원하지 않는 웹입니다.
+      </video>
+
+      <Container className="content">
+        <Text variant="h1">로고넣기</Text>
+        <Button onClick={handleButtonClick} sx={{ opacity: 0.7, border: 'none' }}>
+          들어가기
+        </Button>
+      </Container>
+    </div>
+  );
+}
+
+export default HomePage;
